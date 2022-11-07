368674c58af64763a1bb0564d05d11adbba5cc26
nktrtmv <nvartemov@edu.hse.ru>
2022-11-07 23:51:47 +0300

    +

--- hw1/src/Main.java
+++ hw1/src/Main.java
@@ -2,13 +2,13 @@ import java.util.ArrayList;
 import java.util.Collections;
 
 public class Main {
-    public static boolean isPrime(int num){
+    public static boolean isPrime(int num) {
         if (num == 2 || num == 3) {
             return true;
         }
-        final int q = (int)Math.sqrt(num);
-        for (int i = 2; i <= q; i++){
-            if (num % i == 0){
+        final int q = (int) Math.sqrt(num);
+        for (int i = 2; i <= q; i++) {
+            if (num % i == 0) {
                 return false;
             }
         }
@@ -16,7 +16,7 @@ public class Main {
     }
 
     public static void main(String[] args) {
-        System.out.println("task 1 was completed, IJ installed\n");
+        System.out.println("task 1 was completed, IJ was installed\n");
         System.out.println("task 2\n");
         final int n = 20;
         ArrayList<Double> list = new ArrayList<>(n);
@@ -31,8 +31,8 @@ public class Main {
         System.out.println("average value of arraylist is " + list.get(n / 2) + '\n');
         System.out.println("task 3\n");
 
-        for (int num = 2; num < 100; num++){
-            if (isPrime(num)){
+        for (int num = 2; num < 100; num++) {
+            if (isPrime(num)) {
                 System.out.println(num + " is prime number");
             }
         }
