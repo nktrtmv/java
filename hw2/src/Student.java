87c5909e5b1f9335b820ad313fd3490e1f54e757
nktrtmv <nvartemov@edu.hse.ru>
2022-11-18 22:46:07 +0300

    +

---
+++ hw2/src/Student.java
@@ -0,0 +1,17 @@
+public class Student {
+    String name;
+    String surname;
+
+    public Student(String name, String surname){
+        this.name = name;
+        this.surname = surname;
+    }
+
+    public String name(){
+        return name;
+    }
+
+    public String surname(){
+        return surname;
+    }
+}
